35e97ef4a356291c29d12cde554adc21019b764b
Logan Madden <112110346+ChristopherLoganMadden@users.noreply.github.com>
2024-08-14 07:26:07 -0400

    added to main

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+#Imports
+
 from flask import Flask
+from requests import get
+
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def testServer():
-    return 'Hello, World!'
+
+
+
+def index():
+    ip =get('https://api.ipify.org?format=js').text
+    return "Public IP is " + ip
+
 
 if __name__ == '__main__':
     app.run()
